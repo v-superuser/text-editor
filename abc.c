203fc1c3d8e631fab63851f89e23e40cca6819ac
Vaibhav Bhasin <bhasinvaibhav97@gmail.com>
2017-08-29 14:23:24 +0000

    Add horizontal scrolling

--- abc.c
+++ abc.c
@@ -41,6 +41,7 @@ typedef struct erow {
 struct editorConfig {
     int cx, cy;
     int rowoff;
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
@@ -256,9 +257,10 @@ void editorDrawRows(struct abuf *ab) {
             }
         }
         else {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if(len<0) len = 0;
             if(len>E.screencols) len = E.screencols;
-            abAppend(ab,E.row[filerow].chars,len);
+            abAppend(ab,&E.row[filerow].chars[E.coloff],len);
         }
 
         abAppend(ab,"\x1b[K",3);
@@ -276,6 +278,12 @@ void editorScroll() {
     if(E.cy >= E.rowoff + E.screenrows) {
         E.rowoff = E.cy - E.screenrows + 1;
     }
+    if(E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+    if(E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
+    }
 }
 
 void editorRefreshScreen() {
@@ -289,7 +297,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf,sizeof(buf),"\x1b[%d;%dH",(E.cy - E.rowoff) + 1,E.cx + 1);
+    snprintf(buf,sizeof(buf),"\x1b[%d;%dH",(E.cy - E.rowoff) + 1,(E.cx - E.coloff) + 1);
     abAppend(&ab,buf,strlen(buf));
 
     abAppend(&ab,"\x1b[?25h",6);
@@ -305,7 +313,7 @@ void editorMoveCursor(int key) {
             if(E.cx != 0) E.cx--;
             break;
         case ARROW_RIGHT:
-            if(E.cx != E.screencols - 1) E.cx++;
+            E.cx++;
             break;
         case ARROW_DOWN:
             if(E.cy < E.numrows) E.cy++;
@@ -357,6 +365,7 @@ void initEditor() {
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
